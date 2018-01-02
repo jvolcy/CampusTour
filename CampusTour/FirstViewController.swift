852592663bd9cdf96ea61f8355faddbf5a36a032
jvolcy <jerry.volcy@gmail.com>
2018-01-02 09:02:32 -0500

    Initial Commit

---
+++ CampusTour/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CampusTour
+//
+//  Created by Jerry Volcy on 1/2/18.
+//  Copyright © 2018 Jerry Volcy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CampusTour/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CampusTour
+//
+//  Created by Jerry Volcy on 1/2/18.
+//  Copyright © 2018 Jerry Volcy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
